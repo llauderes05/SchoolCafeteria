c8b78088fe7a1f83a46b709196ff114c53a38055
Llauderes <1820578@ub.edu.ph>
2021-11-30 22:30:56 +0800

    s

--- connection.php
+++ connection.php
@@ -1,16 +1,13 @@
-<?
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'SchoolCafeDB';
+<?php 
+	$servername = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbname = 'SchoolCafeDB';
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    echo "Connection failed! ".$conn->connect_error;
-}
-else{
-    echo "<h1>Connection Successfully!</h1>"
-}
-?>
+	// Create a connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		echo "Connection failed! ".$conn->connect_error;
+	}
+ ?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Main Page</title>
+	<link rel="stylesheet" type="text/css" href="mp.css">
+</head>
+
+<body>
+
+	<section>
+		<div class="banner-text">
+			<h1>WELCOME</h1>
+			<h2>Are you a ... ?</h2>
+
+			<div class="banner-btn">
+				<a href=""><span></span>ADMIN</a>
+				<a href=""><span></span>CUSTOMER</a>
+			</div>
+		</div>
+
+	</section>
+
+</body>
+</html>
