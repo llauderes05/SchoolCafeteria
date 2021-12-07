92836140b3d73b9505823bb3001f8a0440e20f90
1820829 <1820829l@ub.edu.ph>
2021-12-08 01:31:39 +0800

    solema

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Main Page</title>
+	<title>Landing Page</title>
 	<link rel="stylesheet" type="text/css" href="style/mp.css">
 </head>
 
 <body>
+	<div class="banner-text">
+		<h1>WELCOME</h1>
+		<h2>Are you a ... ?</h2>
 
-
+		<div class="banner-btn">
+			<a href="login.php"><span></span>ADMIN</a>
+			<a href=""><span></span>CUSTOMER</a>
+		</div>
+	</div>
 </body>
 </html>
