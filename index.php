e36b2d6ad17533832c2b0ac1f35ce5d84f8a284e
1820829 <1820829l@ub.edu.ph>
2021-12-08 10:01:05 +0800

    solema

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 <head>
 	<title>Landing Page</title>
-	<link rel="stylesheet" type="text/css" href="style/mp.css">
+	<link rel="stylesheet" href="style/mp.css">
 </head>
 
 <body>
-	<div class="banner-text">
-		<h1>WELCOME</h1>
-		<h2>Are you a ... ?</h2>
+	<div class="landingbox">
+		<h1 class="h1">WELCOME</h1>
+		<h2 class="h2">Are you a ... ?</h2>
 
-		<div class="banner-btn">
+		<div class="btn">
 			<a href="login.php"><span></span>ADMIN</a>
 			<a href="login.php"><span></span>CUSTOMER</a>
 		</div>
