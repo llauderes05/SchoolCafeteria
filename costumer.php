c17cf4974b9ef64cd566056c86841356c1ea68b8
Raymond <1820305@ub.edu.ph>
2021-12-14 00:12:54 +0800

    j

--- costumer.php
+++ costumer.php
@@ -94,7 +94,7 @@
             </tbody>
             <tr>
                 <td colspan="4"><form action="index.php" method="post">
-                <input type="submit">
+                <input type="submit" value="Submit">
                 </form></td>
             </tr>
         </table>
