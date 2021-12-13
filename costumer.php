4f7e042083bebbdb13cffb6426658285217129da
Raymond <1820305@ub.edu.ph>
2021-12-13 22:31:16 +0800

    j

--- costumer.php
+++ costumer.php
@@ -70,10 +70,35 @@
                         } else {
                             echo "No result";
                         }
+                        ?>
+            </tbody>
+            <tr>
+                <th colspan="4">PASTA</th>
+            </tr>
+            <tbody>
+            <?php
+                        $sql = "SELECT `ITEM_NAME`, `PRICE`, `STOCKS` FROM `drinks`";
+                        $result = $conn->query($sql);
+                        
+                        if($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                ?>
+                                <tr style="border: solid 1px;">
+                                    <td style="border: solid 1px;"> <?php echo $row["ITEM_NAME"] ?></td>
+                                    <td style="border: solid 1px;"> <?php echo $row["PRICE"] ?></td>
+                                    <td style="border: solid 1px;"> <?php echo $row["STOCKS"] ?></td>
+                                    <td><input type="number"></td>
+                                </tr>
+                                <?php
+                            }
+                        } else {
+                            echo "No result";
+                        }
                         
                         $conn->close();
                         ?>
             </tbody>
+
         </table>
     </div>
 </body>
