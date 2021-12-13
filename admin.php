093adf5a5fd4523c26f59b51d1c5a506bf124764
1820829 <1820829l@ub.edu.ph>
2021-12-13 16:28:10 +0800

    solema

--- admin.php
+++ admin.php
@@ -13,7 +13,7 @@ include_once 'connection.php';
 	<div id="sideNav">
 		<nav>
 			<ul>
-				<li><a href="#">s</a></li>
+				<li><a href="index.php"><</a></li>
 				<li><a href="#">Admin</a></li>
 				<li><a href="#">Meals</a></li>
 				<li><a href="#">Drinks</a></li>
@@ -23,8 +23,27 @@ include_once 'connection.php';
 	</div>
 	<div id="menuBtn">
 		<img src="img/menu.png" id="menu">
-		
 	</div>
+
+	 
+    <script>
+        var menubtn = document.getElementById("menuBtn")
+        var sideNav = document.getElementById("sideNav")
+        var menu = document.getElementById("menu")
+        
+        sideNav.style.right = "-250px";
+        
+        menubtn.onclick = function(){
+            if (sideNav.style.right == "-250px"){
+                sideNav.style.right = "0";
+                menu.src ="img/close.png";
+            }
+            else{
+                sideNav.style.right = "-250px";
+                menu.src ="img/menu.png";
+            }
+        }
+    </script>
 </body>
 </html>
 
