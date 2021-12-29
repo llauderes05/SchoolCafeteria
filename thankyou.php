e1bc24fbb935f487b15b25e7ef9c32435a1ad3a2
Raymond <1820305@ub.edu.ph>
2021-12-29 23:23:32 +0800

    raymond

--- thankyou.php
+++ thankyou.php
@@ -1,13 +1,17 @@
 <html>
     <head>
+    <link rel="stylesheet" href="style/thankyou.css">
         <title>
-
+            Order Succesfully
         </title>
     </head>
     <body>
-        <h1> Order successfully </h1>
-        <p>Order again?</p>
-        <button><a href="costumer.php">Yes</a></button>
-        <button><a href="index.php">No</a></button>
+        <div class="box">
+        <h1> Ordered Succesfully! </h1>
+        <img src="img/check.png" width=135px height=135px>
+        <p>Do you want to order again?</p>
+        <button class="yes"><a href="costumer.php">YES</a></button>
+        <button class="no"><a href="index.php">NO</a></button>
+        </div>
     </body>
 </html>
