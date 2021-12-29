01b3778596086753e2f49fd89ea70f6f3e5df4e6
llauderes05 <1820578@ub.edu.ph>
2021-12-29 20:26:16 +0800

    Llauderes

--- editm.php
+++ editm.php
@@ -10,15 +10,30 @@ $id = $_GET['id'];
     <title>Document</title>
 </head>
 <body>
-        <form action="updatem.php" method="post">
-            <input type="hidden" name="id" required value="<?php echo $id; ?>" ><br>
-			<label>Name</label>
-			<input type="text" name="name" required><br>
-            <label>Price</label>
-            <input type="text" name="price" required><br>
-            <label>Stocks</label>
-            <input type="text" name="stocks" required><br>
-            <input type="submit" value="Update"><br>
- 		</form>
+                    <?php
+                        $sql = "SELECT `name`, `price`, `stocks` FROM `meals` where id = '$id'";
+                        $result = $conn->query($sql);
+                        
+                        if($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                ?>
+                                <h1>Change details for <?php echo $row["name"] ?></h1>
+                                <form action="updatem.php" method="post">
+                                <input type="hidden" name="id" required value="<?php echo $id; ?>" ><br>
+                                <label>Name</label>
+                                <input type="text" name="name" required value="<?php echo $row["name"] ?>"><br>
+                                <label>Price</label>
+                                <input type="text" name="price" required value="<?php echo $row["price"] ?>"><br>
+                                <label>Stocks</label>
+                                <input type="text" name="stocks" required value="<?php echo $row["stocks"] ?>"><br>
+                                <input type="submit" value="Update"><br>
+                            </form>
+                                <?php
+                            }
+                        } else {
+                            echo "No result";
+                        }
+                        ?>
+        
 </body>
 </html>
